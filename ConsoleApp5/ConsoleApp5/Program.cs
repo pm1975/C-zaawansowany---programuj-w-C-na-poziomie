d89a0c6399711ba8694ab9459447fa8f656e822a
pm1975 <pm1975@windowslive.com>
2020-09-14 23:31:51 +0200

    5. Przejscie do podstawowych funkcjonalnosci jezyka. Do 12:30

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //zmienne
+            var someVariable = "string";
+            var someNumber = 5.0m;
+            var trueOrFalse = 5 > 2;
+
+            var date = DateTime.Now;
+
+            //decyzje
+            if (!(5 < 2))
+            {
+
+            }
+
+            //pętle
+            while (someNumber > 2)
+            {
+                someNumber--;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+
+            }
+
+            do
+            {
+
+            } while (false);
+
+            //kolekcje
+            var listOfNumbers = new List<int>();
+        }
+    }
+}
