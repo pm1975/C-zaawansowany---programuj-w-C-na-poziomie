9724799fb3ce8e264b0fa7a8f2d17f57d4d12655
pm1975 <pm1975@windowslive.com>
2020-09-15 22:14:26 +0200

    5. Przejscie... (do minuty 15:00)

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp5
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            //zmienne
+        {
+            #region zmienne
             var someVariable = "string";
             var someNumber = 5.0m;
             var trueOrFalse = 5 > 2;
 
             var date = DateTime.Now;
+            #endregion
 
-            //decyzje
-            if (!(5 < 2))
+            #region kolekcje (listy, tablice)
+            //listy
+            var listOfNumbers = new List<int>
             {
+                5,
+                55,
+                5,
+                55
+            }; //read: new List of int
 
-            }
+            //tablice
+            var listOfNumbers2 = new string[]
+            {
+                "jakis napis", "jakis drugi napis"
+            };
+            var firstString = listOfNumbers2[0];
+            #endregion
 
-            //pętle
+            #region pętle
             while (someNumber > 2)
             {
                 someNumber--;
@@ -35,8 +49,40 @@ namespace ConsoleApp5
 
             } while (false);
 
-            //kolekcje
-            var listOfNumbers = new List<int>();
+            foreach (var number in listOfNumbers)
+            {
+
+            }
+            #endregion
+
+            #region decyzje (if, switch)
+            if (!(5 < 2))
+            {
+
+            }
+
+            //switch - taki if wielokrotny
+            //case'y - czyli przypadki
+            switch (someNumber)
+            {
+                case 5:
+                    {
+
+                    }
+                    break;
+                case 6:
+                    {
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+
+            #region funkcje
+
+            #endregion
         }
     }
 }
