52d45c6c380073eaf90cee07b21d26b44ead208f
PM <pm1975@windowslive.com>
2020-10-01 15:58:29 +0200

    Section 2, Lesson 8. Beginning

---
+++ Sekcja_2/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private int mVariable; //m - member
+
+        public int StatusOfFile { get; set; }
+
+
+        private static void Main(string[] args)
+        {
+            var a = 5 > 0 ? 2 : 5;
+
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool SomeFunction() => true; 
+    }
+}
---
+++ Sekcja_2/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        #region properties
+        private int mVariable;
+
+        public int GetVariable()
+        {
+            return mVariable;
+        }
+
+        public void SetVariable(int someValue)
+        {
+            mVariable = someValue;
+        }
+
+        public int Variable { get; set; }
+
+        #endregion
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    #region NullPointerException
+
+
+
+    #endregion
+}
