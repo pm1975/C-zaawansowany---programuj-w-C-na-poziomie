f5f0bacbe169dd00ffd5129cec376ab8b4df6946
pm1975 <pm1975@windowslive.com>
2020-10-01 21:38:19 +0200

    ConsoleApp7

---
+++ Sekcja_2/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someNumberInString = "5";
+
+            var someNumberAsInt = int.Parse(someNumberInString);
+
+            //someNumberAsInt = int.Parse(Console.ReadLine());
+
+            var isSucceded = int.TryParse(Console.ReadLine(), out var result);
+        }
+    }
+}
