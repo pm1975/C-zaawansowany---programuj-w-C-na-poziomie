04cdc8f57d6e521857f3efec7205719b8d6ecca0
PM <pm1975@windowslive.com>
2020-10-01 15:03:27 +0200

    Sekcja 2, lekcja 7

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isSucceded = DateTime.TryParse(Console.ReadLine(), out var result);
+
+            if (isSucceded)
+            {
+                // Do something
+            }
+            else
+            {
+                Console.WriteLine("Format was wrong, try again");
+            }
+        }
+    }
+}
