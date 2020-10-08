e03ad15769978f99c207bf57a1ea6338ba51afe0
pm1975 <pm1975@windowslive.com>
2020-10-08 22:43:39 +0200

    Section 3. Lesson 11. Abstract class

--- Sekcja_3/ConsoleApp11/ConsoleApp11/Program.cs
+++ Sekcja_3/ConsoleApp11/ConsoleApp11/Program.cs
@@ -6,10 +6,8 @@ namespace ConsoleApp11
     {
         public static void Main(string[] args)
         {
-            var mazda = new Mazda();
-            var porsche = new Porsche();
-
-            var car = new Car();
+            var mazda = new Mazda(100);
+            var porsche = new Porsche(100);
 
             SomeFunction(mazda);
         }
@@ -20,26 +18,50 @@ namespace ConsoleApp11
         }
     }
 
-    public class Car //internal - like public, but access only in this Solution
+    public abstract class Car 
     {
+        public abstract string Name { get; }
         protected int Length { get; set; }
         public int TopSpeed { get; set; }
         public int WheelSize { get; private set; } = 15;
 
-        public void Drive()
-        {
+        public abstract void Drive();
 
+        public Car(int topSpeed)
+        {
+            TopSpeed = topSpeed;
+            Console.WriteLine("I am car constructor");
         }
+
     }
 
-    public class Porsche : Car
+    public sealed class Porsche : Car
     {
+        public override string Name => "I am Porsche";
 
+        public override void Drive()
+        {
+            throw new NotImplementedException();
+        }
+        public Porsche(int topSpeed) : base(topSpeed)
+        {
+            Console.WriteLine("I am Porsche constructor");
+        }
     }
 
     public class Mazda : Car
     {
+        public override string Name => "I am Mazda";
+
         public int SomeMazdaSpecificProperty { get; set; }
+
+        public override void Drive()
+        {
+            throw new NotImplementedException();
+        }
+        public Mazda(int topSpeed) : base(topSpeed)
+        {
+            Console.WriteLine("I am Mazda constructor");
+        }
     }
-}
 }
