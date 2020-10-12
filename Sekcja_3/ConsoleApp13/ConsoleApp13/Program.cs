5e80a54b088eb323b7cac2ae3a9eb207893bf572
PM <pm1975@windowslive.com>
2020-10-12 09:55:53 +0200

    Section 3. Lesson 13. Enum

---
+++ Sekcja_3/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region part1
+            var animationDirection = default(AnimationDirection);
+
+            animationDirection = AnimationDirection.Left;
+
+            var enumString = animationDirection.ToString();
+
+            var enumFromString = Enum.Parse(typeof(AnimationDirection), enumString);
+
+            Animate(animationDirection);
+
+            foreach (var enumValue in Enum.GetValues(typeof(AnimationDirection)))
+                Console.WriteLine(enumValue);
+            #endregion
+
+            // | and & - flags
+
+            // one | is as + in strings
+            var userPermissions = UserPermissions.Create | UserPermissions.Delete;
+
+            //read if userPermissions contain UserPermissions.Create
+            if ((userPermissions & UserPermissions.Create) == UserPermissions.Create)
+            {
+
+            }
+        }
+        #region part1
+        public static void Animate(AnimationDirection animationDirection)
+        {
+            Console.WriteLine($"Animuje teraz obrazek z {animationDirection.ToString()}");
+        }
+        #endregion
+    }
+    #region part1
+    public enum AnimationDirection
+    {
+        Unknown = 0,
+        Left,
+        Right,
+        Up,
+        Down
+    }
+    #endregion
+
+    public enum UserPermissions
+    {
+        Create = 1, 
+        Delete = 2,
+        Write = 4,
+        Read = 8
+    }
+}
