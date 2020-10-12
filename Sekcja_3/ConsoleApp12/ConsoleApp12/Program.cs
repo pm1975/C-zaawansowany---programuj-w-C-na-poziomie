87ca0857391b5cb5794180df3c43c69952259456
pm1975 <pm1975@windowslive.com>
2020-10-12 07:54:33 +0200

    ConsoleApp12

---
+++ Sekcja_3/ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    public class Program
+    {
+        public static int Counter;
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            Console.WriteLine(Helpers.AddTwoStrings("ad", "sdaf"));
+           
+        }
+
+        public void SomeFunction()
+        {
+            Counter++;
+        }
+    }
+
+    public static class Helpers
+    {
+        public static string AddTwoStrings(string first, string second)
+            => first + second;
+    }
+}
