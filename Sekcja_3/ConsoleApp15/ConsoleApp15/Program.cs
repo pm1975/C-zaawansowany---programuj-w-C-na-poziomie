f2086dcd9a78efa7edd3f197714ad0aaa9e7991e
PM <pm1975@windowslive.com>
2020-10-13 15:44:14 +0200

    App15 start

---
+++ Sekcja_3/ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someClass = new MyClass<int>();
+            _ = someClass.MyNumber;
+        }
+    }
+
+    public class MyClass<T>
+    {
+        public T MyNumber { get; set; }
+    }
+}
