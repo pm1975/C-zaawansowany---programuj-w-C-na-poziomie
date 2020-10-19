c8ef8e044f4082723ec433c3ff66c4fce5967655
pm1975 <pm1975@windowslive.com>
2020-10-19 23:21:43 +0200

    Lesson 18 Linq to 20 minutes

---
+++ Sekcja_4/ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region funkcja anonimowa
+            var a = new
+            {
+                ValueOfSomething = 5,
+                ValueOfSomethingElse = "safdsf"
+            };
+            #endregion
+
+            var listOfInts = new List<int>
+            {
+                2, 5, 8, 1, 10, 15
+            };
+
+            var listOfStrings = new List<string>
+            {
+                "cos", "tam", "jest", "fajnego"
+            };
+
+            var listOfUsers = new List<User>
+            {
+                new User
+                {
+                    Name = "Patryk",
+                    Age = 21,
+                    Email = "przykladowy@email.com"
+                },
+                new User
+                {
+                    Name = "Tomek",
+                    Age = 24,
+                    Email = "przykladowy@email.com"
+                },
+                new User
+                {
+                    Name = "Krzysiek",
+                    Age = 240,
+                    Email = "przykladowy2@email.com"
+                }
+            };
+
+            foreach (var valueInt in listOfInts.Where(x => x > 5))
+            {
+                    Console.WriteLine(valueInt);
+            }
+
+            var someInts = listOfInts.Where(x => x > 5).ToList();
+
+            listOfInts.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+
+            Console.WriteLine(listOfInts.Average(x => x));
+            Console.WriteLine(listOfInts.FirstOrDefault(x => x > 30));
+            Console.WriteLine(listOfInts.FirstOrDefault(x => x > 5));
+            Console.WriteLine(listOfInts.Where(x => x > 5).Skip(1).FirstOrDefault());
+            Console.WriteLine(listOfUsers.Average(x => x.Age));
+        }
+    }
+}
