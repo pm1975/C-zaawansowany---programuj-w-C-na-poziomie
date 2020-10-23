d25d9cd0fafd89d2dbf52c43a8c0485a4f972ffa
pm1975 <pm1975@windowslive.com>
2020-10-23 22:45:57 +0200

    Section 4. Lesson 20.

--- Sekcja_4/ConsoleApp19/ConsoleApp19/Program.cs
+++ Sekcja_4/ConsoleApp19/ConsoleApp19/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp19
 {
@@ -6,7 +8,30 @@ namespace ConsoleApp19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var someStrings = new List<string>();
+
+            someStrings.SomeFunction();
+
+            someStrings.Where(x => x.Length > 2);
+
+            var a = "555";
+            a.SomeFunction3();
+        }
+    }
+    //Extension methods mogą być użyte tylko w statycznych metodach.
+    //I to jest bardzo ważna uwaga.
+    public static class MyClass
+    {
+        //sdasf
+
+        public static void SomeFunction(this List<string> someList)
+        {
+
+        }
+
+        public static void SomeFunction3(this string someString2)
+        {
+
         }
     }
 }
---
+++ Sekcja_4/consoleApp20/consoleApp20/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace consoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IEnumerable, IQueryable, ICollection, IList
+            var list = new List<int>();
+            IEnumerable enumerable;
+            IQueryable
+            foreach (var variable in list)
+            {
+
+            }
+
+            PrintAllElements(list);
+        }
+
+        private static void PrintAllElements(IList<int> list)
+        {
+            list.Add(2);
+
+            var element2 = list[0];
+
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+    #region MyEnumerable
+    public class MyEnumerable<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+    #region MyCollection
+    public class MyCollection<T> : ICollection<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+    #region MyList
+    public class MyList<T> : IList<T>
+    {
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
