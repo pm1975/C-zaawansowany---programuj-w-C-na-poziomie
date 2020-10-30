8918a55819ebc24118aac01fb4ccacff57c5af06
pm1975 <pm1975@windowslive.com>
2020-10-31 00:58:28 +0100

    Section 4. Lesson 22. Dependency Injection

---
+++ Sekcja_4/ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        public static ILogger GetLogger()
+        {
+            return new ConsoleLogger();
+        }
+
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddTransient<ILogger, FileLogger>();
+
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddJsonFile("appsettings.json", true);
+
+            var configuration = configurationBuilder.Build();
+
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger>();
+        }
+    }
+
+    public interface ILogger
+    {
+        public int SomeValue { get; set; }
+    }
+
+    public class FileLogger : ILogger
+    {
+        public int SomeValue { get; set; }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public int SomeValue { get; set; }
+    }
+}
---
+++ Sekcja_4/ConsoleApp22/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IServiceProvider _logger2;
+
+        public HomeController(ILogger<HomeController> logger, IServiceProvider logger2)
+        {
+            _logger = logger;
+            _logger2 = logger2;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
