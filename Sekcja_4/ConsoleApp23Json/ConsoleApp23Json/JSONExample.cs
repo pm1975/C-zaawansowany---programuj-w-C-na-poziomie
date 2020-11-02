9840785aff1bb159ba773d4cc649431726e99127
pm1975 <pm1975@windowslive.com>
2020-11-02 20:58:13 +0100

    Sekcja 4. 23. Json

---
+++ Sekcja_4/ConsoleApp23Json/ConsoleApp23Json/JSONExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp23Json
+{
+    public class JSONExample
+    {
+        public string Username { get; set; }
+
+        public List<string> SomeStrings { get; set; }
+
+        public int Value { get; set; }
+    }
+}
---
+++ Sekcja_4/ConsoleApp23Json/ConsoleApp23Json/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ConsoleApp23Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someObject = new JSONExample
+            {
+                Username = "Some Username",
+                SomeStrings = new System.Collections.Generic.List<string>
+                {
+                    "a", "s"
+                },
+                Value = 5
+            };
+
+            var json = JsonConvert.SerializeObject(someObject);
+
+            Console.WriteLine(json);
+
+            var someOtherObject = JsonConvert.DeserializeObject<JSONExample>(json);
+        }
+
+        public void SomeApiFunciton(JSONExample dfasdf)
+        {
+
+        }
+    }
+
+}
