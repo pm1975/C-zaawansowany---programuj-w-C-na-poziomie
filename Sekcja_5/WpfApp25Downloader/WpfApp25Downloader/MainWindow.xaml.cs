543d66fc941f7dcbbb833ee81fed0bd769a4c848
PM <pm1975@windowslive.com>
2020-11-04 16:38:43 +0100

    Section 5, lessson 35 to 13 minutes

---
+++ Sekcja_5/WpfApp25Downloader/WpfApp25Downloader/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Threading;
+using System.Windows;
+
+namespace WpfApp25Downloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var webClient = new WebClient();
+
+            //webClient.DownloadFile(WebsiteUrl.Text, "przykladowy");
+            var downloadString = webClient.DownloadString(WebsiteUrl.Text);
+
+            Thread.Sleep(2000);
+        }
+    }
+}
